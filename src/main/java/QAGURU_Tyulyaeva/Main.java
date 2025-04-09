136f888273caf7dc3345824f30b1732a48898c92
Tyulyaeva Inna <tyulyaeva.inna@yandex.ru>
2025-04-09 17:25:04 +0300

    private static String msg

--- src/main/java/QAGURU_Tyulyaeva/Main.java
+++ src/main/java/QAGURU_Tyulyaeva/Main.java
@@ -4,7 +4,7 @@ package QAGURU_Tyulyaeva;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
 
-    private static String msg = "Hello, QAGURU_Tyulyaeva))))!!!"
+    private static String msg = "Hello, QAGURU_Tyulyaeva))))!!!";
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
