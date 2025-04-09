5306a0301bd43de9c3e5edd2f6c2fad1865429a1
Tyulyaeva Inna <tyulyaeva.inna@yandex.ru>
2025-04-09 17:24:20 +0300

    private static String msg = "Hello, QAGURU_Tyulyaeva))))!!!"

--- src/main/java/QAGURU_Tyulyaeva/Main.java
+++ src/main/java/QAGURU_Tyulyaeva/Main.java
@@ -3,6 +3,8 @@ package QAGURU_Tyulyaeva;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+
+    private static String msg = "Hello, QAGURU_Tyulyaeva))))!!!"
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
