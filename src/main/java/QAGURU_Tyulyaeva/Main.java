b9f5042e5a54f5b49f442ea29d7884966aeda817
tyulyaeva <72944548+tyulyaeva@users.noreply.github.com>
2025-04-09 17:47:22 +0300

    Update Main.java

--- src/main/java/QAGURU_Tyulyaeva/Main.java
+++ src/main/java/QAGURU_Tyulyaeva/Main.java
@@ -3,8 +3,7 @@ package QAGURU_Tyulyaeva;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter
 
-public class Main {
-private static String msg = "Hello, QAGURU_Tyulyaeva))))"
+public class Main 
     
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
