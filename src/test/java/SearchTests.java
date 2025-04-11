1e32c8d39036f09ffbe4c93bb553ba4c0933eb68
Tyulyaeva Inna <tyulyaeva.inna@yandex.ru>
2025-04-11 11:45:21 +0300

    QAGURU_Tyulyaeva4

--- src/test/java/SearchTests.java
+++ src/test/java/SearchTests.java
@@ -1,24 +1,22 @@
 package tests;
 
-import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebElement;
 
-import static com.codeborne.selenide.Selectors.byName;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
-import static org.openqa.selenium.Keys.BACK_SPACE;
 import static org.openqa.selenium.Keys.SPACE;
 
 public class SearchTests {
 
     @BeforeAll
     static void beforeAll(){
+        Configuration.browserSize = "1920x1080";
         Configuration.baseUrl = "https://demoqa.com/";
         Configuration.pageLoadStrategy ="eager";
     }
@@ -34,20 +32,29 @@ public class SearchTests {
         $("#firstName").setValue("Инна");
         $("#lastName").setValue("Тюляева");
         $("#userEmail").setValue("tyulyaeva.inna@yandex.ru");
-        $("#genterWrapper > div.col-md-9.col-sm-12 > div:nth-child(2) > label").click();
+        $("#genterWrapper").$(byText("Female")).click();
         $("#userNumber").setValue("9178332203");
         $("[id=dateOfBirthInput]").sendKeys(Keys.CONTROL + "A");
         $("[id=dateOfBirthInput]").sendKeys(SPACE);
         $("#dateOfBirthInput").setValue("20 Jul 1991").pressEnter();
         $("#subjectsInput").setValue("Arts").pressEnter();
-        $("#hobbiesWrapper > div.col-md-9.col-sm-12 > div:nth-child(2) > label").click();
-
+        $("#hobbiesWrapper").$(byText("Reading")).click();
         $("#uploadPicture").uploadFromClasspath("retouch.jpg");
         $("#currentAddress").setValue("Санкт-Петербург, п. Парглово").pressEnter();
         $("#react-select-3-input").setValue("Uttar Pradesh").pressEnter();
         $("#react-select-4-input").setValue("Agra").pressEnter();
         $("#submit").click();
 
-        sleep(5000);
+
+        $(".table-responsive").$(byText("Student Name")).parent().shouldHave(text("Инна Тюляева"));
+        $(".table-responsive").$(byText("Student Email")).parent().shouldHave(text("tyulyaeva.inna@yandex.ru"));
+        $(".table-responsive").$(byText("Gender")).parent().shouldHave(text("Female"));
+        $(".table-responsive").$(byText("Mobile")).parent().shouldHave(text("9178332203"));
+        $(".table-responsive").$(byText("Date of Birth")).parent().shouldHave(text("20 July,1991"));
+        $(".table-responsive").$(byText("Subjects")).parent().shouldHave(text("Arts"));
+        $(".table-responsive").$(byText("Hobbies")).parent().shouldHave(text("Reading"));
+        $(".table-responsive").$(byText("Picture")).parent().shouldHave(text("retouch.jpg"));
+        $(".table-responsive").$(byText("Address")).parent().shouldHave(text("Санкт-Петербург, п. Парглово"));
+        $(".table-responsive").$(byText("State and City")).parent().shouldHave(text("Uttar Pradesh Agra"));
     }
 }
