5949fa19dfbcb54aa77b9f59a297d9a222cbcac2
Tyulyaeva Inna <tyulyaeva.inna@yandex.ru>
2025-07-16 20:59:16 +0300

    rest_api_3

--- src/test/java/config/ApiConfig.java
+++ src/test/java/config/ApiConfig.java
@@ -11,28 +11,4 @@ public interface ApiConfig extends Config {
     @Key("basePath")
     @DefaultValue("/api")
     String basePath();
-
-    @Key("userLogin")
-    String userLogin();
-
-    @Key("userPath")
-    String userPath();
-
-    @Key("userRegister")
-    String userRegister();
-
-    @Key("apiKey")
-    String apiKey();
-
-    @Key("apiKeyName")
-    String apiKeyName();
-
-    @Key("userPass")
-    String userPass();
-
-    @Key("userEmail")
-    String userEmail();
-
-    @Key("userPassLogin")
-    String userPassLogin();
 }
